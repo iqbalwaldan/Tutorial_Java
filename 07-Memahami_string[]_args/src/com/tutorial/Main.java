931316e99a194e78498d08ea8c31b200ce618eeb
iqbalwaldan <iqbalwaldan0510@gmaail.com>
2020-11-23 17:38:09 +0700

    07-Memahami_String[]_args

---
+++ 07-Memahami_string[]_args/src/com/tutorial/Main.java
@@ -0,0 +1,10 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hallo " + args[0] + " Ganteng");
+        System.out.println("hallo " + args[1] + " Manise");
+    }
+}
