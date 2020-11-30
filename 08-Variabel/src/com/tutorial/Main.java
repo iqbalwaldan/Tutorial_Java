642d114af7a0c49f3735fa689c121bbe4a0e8d5f
iqbalwaldan <iqbalwaldan0510@gmaail.com>
2020-11-30 07:58:25 +0700

    08-Variabel

---
+++ 08-Variabel/src/com/tutorial/Main.java
@@ -0,0 +1,26 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // ini adalah komen
+        /* banyak
+        komen */
+
+        // kita akan akan membuat variabel
+        // tipe data 
+        int a = 10; // assignment
+        System.out.println("nilai a = " + a);
+
+        a = 20;
+        System.out.println("nilai a baru = " + a);
+
+        // kita akan membuat sebuah deklarasi
+        int b; // ini adalah deklarasi
+
+        b = 7;
+
+        System.out.println("nilai b = " + b);
+    }
+}
