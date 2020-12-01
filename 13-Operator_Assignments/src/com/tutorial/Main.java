e14e20b5f3b88ad14ddf7212681d6656fa47afd2
iqbalwaldan <iqbalwaldan0510@gmaail.com>
2020-12-01 11:13:10 +0700

    13-Operator_Assignments

---
+++ 13-Operator_Assignments/src/com/tutorial/Main.java
@@ -0,0 +1,34 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Operator Assignment
+
+        // Assignment Operator jumlah
+        int a = 1;
+        a += 10; // a = a + 10
+        System.out.println("Nilai a = " + a);
+
+        // Assignment Operator pengurangan
+        int b = 100;
+        b -= 25;
+        System.out.println("Nilai b = " + b);
+
+        // Assignment Operator perkalian
+        int c = 100;
+        c *= 20;
+        System.out.println("Nilai c = " + c);
+
+        // Assignment Operator pembagian
+        int d = 100;
+        d /= 20;
+        System.out.println("Nilai d = " + d);
+
+        // Assignment Operator pengurangan
+        int e = 10;
+        e %= 7;
+        System.out.println("Nilai e = " + e);
+    }
+}
