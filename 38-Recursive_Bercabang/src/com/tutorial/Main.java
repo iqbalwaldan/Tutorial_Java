f14c34db61854039863beaff314abfb8083cb916
iqbalwaldan <iqbalwaldan0510@gmail.com>
2021-07-03 15:50:32 +0700

    39-Overload_Fungsi

--- 38-Recursive_Bercabang/src/com/tutorial/Main.java
+++ 38-Recursive_Bercabang/src/com/tutorial/Main.java
@@ -1,6 +1,7 @@
 package com.tutorial;
 
-import java.util.Scanner;
+import java.util.*;
+import java.lang.String;
 
 public class Main {
 
@@ -9,17 +10,19 @@ public class Main {
         Scanner userInput = new Scanner(System.in);
         System.out.print("masukan nilai n = ");
         int nilai = userInput.nextInt();
-        int nilai_fibonacci = fibonacci(nilai);
+        int nilai_fibonacci = fibonacci(nilai, "atas");
         System.out.println("nilai fibonacci ke-"+ nilai +" adalah " + nilai_fibonacci);
     }
 
 
-    private static int fibonacci(int n){
+    private static int fibonacci(int n, String daun){
+        System.out.println("daun " + daun);
+        System.out.println("Fibonacci ke - " + n);
         if (n == 1 || n == 0){
             return n;
         }
         else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
+            return fibonacci(n - 1, "kiri") + fibonacci(n - 2, "kanan");
         }
     }
 
